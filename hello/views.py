9bf3dc78e04581485ce2663ac7f2038e05b57d87
Kevin Tetz <kevin.tetz@gmail.com>
2024-09-02 16:41:24 -0600

    update

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,10 @@
+import re
+
 from django.shortcuts import redirect, render
 from django.utils.timezone import datetime
 from django.views.generic import ListView
+
+from django.http import HttpResponse
 
 from hello.forms import LogMessageForm
 from hello.models import LogMessage
@@ -26,11 +30,30 @@ def contact(request):
     return render(request, "hello/contact.html")
 
 
+def hello_there2(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
+
+
+
 def hello_there(request, name):
     """Renders the hello_there page.
     Args:
         name: Name to say hello to
     """
+
     return render(
         request, "hello/hello_there.html", {"name": name, "date": datetime.now()}
     )
